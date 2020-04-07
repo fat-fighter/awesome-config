ece1d2cfd87e996d3ebf88c28701504fd48f45e1
Gurpreet Singh <g.singh6343@gmail.com>
2020-04-07 03:14:03 -0400

    Added borders as titlebars feature

--- components/titlebar.lua
+++ components/titlebar.lua
@@ -15,7 +15,7 @@ local helpers   = require("helpers")
 -- Adding Button Controls
 
 local buttons = gears.table.join(
-    -- Left button - move
+    -- Left button - move {{
     awful.button(
         {}, 1,
         function()
@@ -27,8 +27,9 @@ local buttons = gears.table.join(
             awful.mouse.client.move(c)
         end
     ),
+    -- }}
 
-    -- Middle button - close
+    -- Middle button - close {{
     awful.button(
         {}, 2,
         function ()
@@ -36,8 +37,9 @@ local buttons = gears.table.join(
             window_to_kill:kill()
         end
     ),
+    -- }}
 
-    -- Right button - resize
+    -- Right button - resize {{
     awful.button(
         {}, 3,
         function()
@@ -47,111 +49,172 @@ local buttons = gears.table.join(
             awful.mouse.client.resize(c)
         end
     )
+    -- }}
 )
 
 --------------------------------------------------------------------------------
 -- Creating a Titlebar
 
-client.connect_signal(
-    "request::titlebars",
-    function(c)
-        local title_widget
+local positions
+if beautiful.imitate_borders then
+    positions = {"top", "bottom", "left", "right"}
+else
+    positions = {beautiful.position}
+end
 
-        if beautiful.title.enabled then
-            title_widget       = awful.titlebar.widget.titlewidget(c)
-            title_widget.font  = beautiful.title.font
-            title_widget.align = beautiful.title.align
-        else
-            title_widget = wibox.widget.textbox("")
-        end
+if beautiful.enabled then
+    client.connect_signal(
+        "request::titlebars",
+        function(c)
+            for _, pos in ipairs(positions) do
+                local size
+                local title_text
+                local item_layout = "horizontal"
 
-        local titlebar = awful.titlebar(
-            c,
-            {
-                font      = beautiful.font,
-                size      = beautiful.size,
-                position  = beautiful.position,
-				bg_focus  = beautiful.bg_focus,
-				bg_normal = beautiful.bg_normal
-            }
-        )
+                if pos == beautiful.position then
+                    -- For main titlebar
 
-        local item_layout = "horizontal"
-        if position == "left" or position == "right" then
-            item_layout = "vertical"
-        end
+                    -- Title text widget {{
+                    if beautiful.title.enabled then
+                        title_text = {
+                            font   = beautiful.title.font,
+                            align  = beautiful.title.align,
+                            widget = awful.titlebar.widget.titlewidget(c),
+                        }
+                    end
+                    -- }}
 
-        titlebar.sticky = wibox.widget {
-            {
-                image         = beautiful.sticky_icon,
-                forced_height = beautiful.icon_size,
-                forced_width  = beautiful.icon_size,
-                widget        = wibox.widget.imagebox
-            },
-            visible = c.sticky,
-            right   = beautiful.icon_spacing,
-            widget  = wibox.container.margin
-        }
-        titlebar.ontop = wibox.widget {
-            {
-                image         = beautiful.ontop_icon,
-                forced_height = beautiful.icon_size,
-                forced_width  = beautiful.icon_size,
-                widget        = wibox.widget.imagebox
-            },
-            visible = c.ontop,
-            right   = beautiful.icon_spacing,
-            widget  = wibox.container.margin
-        }
+                    size = beautiful.size
 
-        titlebar:setup {
-            {
-                {
-                    title_widget,
-                    buttons = buttons,
-                    layout  = wibox.layout.flex.horizontal
-                },
-                left   = beautiful.padding,
-                right  = beautiful.padding,
-                widget = wibox.container.margin
-            },
-            {
-                nil, nil,
-                helpers.center_align_widget(
+                    -- Different layouts for positions {{
+                    if pos == "left" or pos == "right" then
+                        item_layout = "vertical"
+                        center_layout = "horizontal"
+                    end
+                    -- }}
+                else
+                    -- For borders as titlebars
+                    size = beautiful.border_size
+                end
+
+                -- Create titlebar {{
+                local titlebar = awful.titlebar(
+                    c,
                     {
-                        titlebar.sticky,
-                        titlebar.ontop,
-                        layout  = wibox.layout.fixed[item_layout]
-                    },
-                    "vertical"
-                ),
-                layout = wibox.layout.align[item_layout]
-            },
-            layout = wibox.layout.stack
-        }
+                        font      = beautiful.font,
+                        size      = size,
+                        position  = pos,
+                        bg_focus  = beautiful.bg_focus,
+                        bg_normal = beautiful.bg_normal
+                    }
+                )
+                -- }}
 
-        c.titlebar = titlebar
-    end
-)
+                local sticky, ontop
+
+                -- Add icons for main titlebar {{
+                if pos == beautiful.position then
+                    titlebar.sticky = wibox.widget {
+                        {
+                            image         = beautiful.sticky_icon,
+                            forced_height = beautiful.icon_size,
+                            forced_width  = beautiful.icon_size,
+                            widget        = wibox.widget.imagebox
+                        },
+                        visible = c.sticky,
+                        margins   = beautiful.icon_spacing,
+                        widget  = wibox.container.margin
+                    }
+                    titlebar.ontop = wibox.widget {
+                        {
+                            image         = beautiful.ontop_icon,
+                            forced_height = beautiful.icon_size,
+                            forced_width  = beautiful.icon_size,
+                            widget        = wibox.widget.imagebox
+                        },
+                        visible = c.ontop,
+                        margins = beautiful.icon_spacing,
+                        widget  = wibox.container.margin
+                    }
+                end
+                -- }}
+
+                -- Rotate titlebar contents for different positions {{
+                local direction = "north"
+                if pos == "right" then
+                    direction = "west"
+                elseif pos == "left" then
+                    direction = "east"
+                end
+                -- }}
+
+                -- Wrapper for title {{
+                local title_widget = wibox.container {
+                    {
+                        {
+                            title_text,
+                            buttons = buttons,
+                            layout  = wibox.layout.flex[item_layout]
+                        },
+                        left   = beautiful.margin,
+                        right  = beautiful.margin,
+                        widget = wibox.container.margin
+                    },
+                    direction = direction,
+                    widget    = wibox.container.rotate
+                }
+                -- }}
+
+                -- Setup titlebar {{
+                titlebar:setup {
+                    title_widget,
+                    {
+                        nil, nil,
+                        helpers.center_align_widget(
+                            {
+                                titlebar.sticky,
+                                titlebar.ontop,
+                                layout  = wibox.layout.fixed[item_layout]
+                            },
+                            center_layout
+                        ),
+                        layout = wibox.layout.align[item_layout]
+                    },
+                    layout = wibox.layout.stack
+                }
+                -- }}
+
+                if pos == beautiful.pos then
+                    -- Set main titlebar as client titlebar
+                    c.titlebar = titlebar
+                end
+            end
+        end
+    )
+end
 
 --------------------------------------------------------------------------------
 -- Adding Connect Signals to Update Titlebar Indicators
 
-client.connect_signal(
-    "property::sticky",
-    function(c)
-        c.titlebar.sticky.visible = c.sticky
-    end
-)
-client.connect_signal(
-    "property::ontop",
-    function(c)
-        c.titlebar.ontop.visible = c.ontop
-    end
-)
+if beautiful.enabled then
+    -- Property handler for sticky {{
+    client.connect_signal(
+        "property::sticky",
+        function(c)
+            c.titlebar.sticky.visible = c.sticky
+        end
+    )
+    -- }}
 
--- Disable popup tooltip on titlebar button hover
-awful.titlebar.enable_tooltip = beautiful.tooltip
+    -- Property handler for ontop {{
+    client.connect_signal(
+        "property::ontop",
+        function(c)
+            c.titlebar.ontop.visible = c.ontop
+        end
+    )
+    -- }}
+end
 
 --------------------------------------------------------------------------------
 return titlebars
